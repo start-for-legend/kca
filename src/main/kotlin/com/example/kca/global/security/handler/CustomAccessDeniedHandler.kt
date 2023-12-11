a9fefde80299170421090aa8c30b2670ba346a19
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-12-11 09:27:56 +0900

    add :: custom filter

---
+++ src/main/kotlin/com/example/kca/global/security/handler/CustomAccessDeniedHandler.kt
@@ -0,0 +1,17 @@
+package com.example.kca.global.security.handler
+
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class CustomAccessDeniedHandler : AccessDeniedHandler{
+
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?
+    ) {
+        response?.sendError(HttpServletResponse.SC_FORBIDDEN)
+    }
+}
